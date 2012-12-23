09b57fa31c6f20739e61c03831e055b0fd79edcf
Ted Blackman <ted.blackman@gmail.com>
2012-12-23 16:59:03 -0500

    Update evaltest.js
    changed the function name so it's not the same as the built-in

--- evaltest.js
+++ evaltest.js
@@ -2,7 +2,7 @@ if (Meteor.isClient) {
   Template.hello.events({
     'click #run' : function (e,template) {
       var code = $(template.find('textarea')).val();
-      Meteor.call('eval',code,function(error,result){
+      Meteor.call('serverEval',code,function(error,result){
         error && console.error(error);
         result && console.log(result);
       });
@@ -12,7 +12,7 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.methods({
-    eval: function(code) {
+    serverEval: function(code) {
       eval(code);
     }
   });
